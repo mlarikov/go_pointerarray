20cf2c9d360aa4e24a55a199b41e31698fecc64c
Mikhail Larikov <mlarikov@gmail.com>
2022-09-27 09:22:59 +0300

    Added print example

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func modifyArray(arr *[3][3]string) {
 	arr[2][2] = "hello"
+	fmt.Println("Arr", reflect.TypeOf(arr), arr)
 }
 
 func main() {
 	myArr := [3][3]string{{"qwe", "asd", "zxc"}, {"rty", "fgh", "vbn"}, {"uio", "jkl", "m,."}}
+	fmt.Println("myArr", reflect.TypeOf(myArr), myArr)
 	modifyArray(&myArr)
-	fmt.Println(myArr)
+	fmt.Println("myArr", reflect.TypeOf(myArr), myArr)
 }
